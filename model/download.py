3796e4badabc0a50693c25009160040bb2b859cf
FanYu <32595791+FutureYu@users.noreply.github.com>
2019-07-23 13:22:53 +0800

    add model

--- model/download.py
+++ model/download.py
@@ -1,13 +1,21 @@
 import requests
 
 base_url = "https://storage.googleapis.com/quickdraw-models/sketchRNN/models/"
-
+headers = {
+    "DNT": "1",
+    "Sec-Fetch-Mode": "cors",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3843.0 Safari/537.36 Edg/77.0.218.4"
+}
 availableModels = ['bird', 'ant','ambulance','angel','alarm_clock','antyoga','backpack','barn','basket','bear','bee','beeflower','bicycle','book','brain','bridge','bulldozer','bus','butterfly','cactus','calendar','castle','cat','catbus','catpig','chair','couch','crab','crabchair','crabrabbitfacepig','cruise_ship','diving_board','dog','dogbunny','dolphin','duck','elephant','elephantpig','everything','eye','face','fan','fire_hydrant','firetruck','flamingo','flower','floweryoga','frog','frogsofa','garden','hand','hedgeberry','hedgehog','helicopter','kangaroo','key','lantern','lighthouse','lion','lionsheep','lobster','map','mermaid','monapassport','monkey','mosquito','octopus','owl','paintbrush','palm_tree','parrot','passport','peas','penguin','pig','pigsheep','pineapple','pool','postcard','power_outlet','rabbit','rabbitturtle','radio','radioface','rain','rhinoceros','rifle','roller_coaster','sandwich','scorpion','sea_turtle','sheep','skull','snail','snowflake','speedboat','spider','squirrel','steak','stove','strawberry','swan','swing_set','the_mona_lisa','tiger','toothbrush','toothpaste','tractor','trombone','truck','whale','windmill','yoga','yogabicycle']
-
-for model in availableModels:
-    model_url = model + ".gen.json"
-    url = base_url
-    res = requests.get(url)
-    with open(model_url, "w") as f:
-        f.write(res.text)
-    print(model + " get!")
+with open("list.txt", "w") as f:
+    for model in availableModels:
+        model_url = model + ".gen.json"
+        url = base_url + model_url
+        f.write(url + "\n")
+        # res = requests.get(url, headers=headers)
+        # if res.status_code == 200:
+        #     with open(model_url, "w") as f:
+        #         f.write(res.text)
+        #     print(model + " get!")
+        # else:
+        #     print(model + " fail!")
