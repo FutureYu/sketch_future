f05eb3e4ace2593431b97653813113366e20d5e9
FanYu <32595791+FutureYu@users.noreply.github.com>
2019-07-23 12:56:54 +0800

    init

---
+++ model/download.py
@@ -0,0 +1,13 @@
+import requests
+
+base_url = "https://storage.googleapis.com/quickdraw-models/sketchRNN/models/"
+
+availableModels = ['bird', 'ant','ambulance','angel','alarm_clock','antyoga','backpack','barn','basket','bear','bee','beeflower','bicycle','book','brain','bridge','bulldozer','bus','butterfly','cactus','calendar','castle','cat','catbus','catpig','chair','couch','crab','crabchair','crabrabbitfacepig','cruise_ship','diving_board','dog','dogbunny','dolphin','duck','elephant','elephantpig','everything','eye','face','fan','fire_hydrant','firetruck','flamingo','flower','floweryoga','frog','frogsofa','garden','hand','hedgeberry','hedgehog','helicopter','kangaroo','key','lantern','lighthouse','lion','lionsheep','lobster','map','mermaid','monapassport','monkey','mosquito','octopus','owl','paintbrush','palm_tree','parrot','passport','peas','penguin','pig','pigsheep','pineapple','pool','postcard','power_outlet','rabbit','rabbitturtle','radio','radioface','rain','rhinoceros','rifle','roller_coaster','sandwich','scorpion','sea_turtle','sheep','skull','snail','snowflake','speedboat','spider','squirrel','steak','stove','strawberry','swan','swing_set','the_mona_lisa','tiger','toothbrush','toothpaste','tractor','trombone','truck','whale','windmill','yoga','yogabicycle']
+
+for model in availableModels:
+    model_url = model + ".gen.json"
+    url = base_url
+    res = requests.get(url)
+    with open(model_url, "w") as f:
+        f.write(res.text)
+    print(model + " get!")
